da2012e9848831478b733802f63adbe87cea6adf
aleksandarmihaylov <aleksandaryanev95@gmail.com>
2017-11-10 13:26:25 +0100

    Init the database

---
+++ MusicStore/Models/MusicStoreDataContextInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MusicStore.Models
+{
+    public class MusicStoreDataContextInitializer : DropCreateDatabaseAlways<MusicStoreDataContext>
+    {
+        protected override void Seed(MusicStoreDataContext context)
+        {
+            Artist artist = new Artist() { Name = "First Artist" };
+            context.Artists.Add(artist);
+
+            context.Artists.Add(new Artist() { Name = "Second Artist" });
+
+            context.SaveChanges();
+        }
+    }
+}
