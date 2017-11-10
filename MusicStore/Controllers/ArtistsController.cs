031e8fd8b2b51e29e69ef66204dc7733f8c21cb2
aleksandarmihaylov <aleksandaryanev95@gmail.com>
2017-11-10 13:16:05 +0100

    Working with migrations and repositories in EF

---
+++ MusicStore/Controllers/ArtistsController.cs
@@ -0,0 +1,35 @@
+﻿using MusicStore.Models;
+using MusicStore.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class ArtistsController : Controller
+    {
+        //MusicStoreDataContext context = new MusicStoreDataContext();
+        ArtistRepository repository = new ArtistRepository();
+        // GET: Artist
+        public ActionResult Index()
+        {
+            return View(repository.GetAll());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Artist artist)
+        {
+            if (!ModelState.IsValid) return View(artist);
+            repository.Add(artist);
+            repository.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
